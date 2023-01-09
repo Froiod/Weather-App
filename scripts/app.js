ce59916d77dfa3c831bd6b30e3e2fd488f29bdfe
Paolo Guray <froiodpaolo@gmail.com>
2023-01-09 20:19:00 +0800

    update scripts

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 const cityForm = document.querySelector('form')
 const card = document.querySelector('.card')
 const details = document.querySelector('.details')
+const time = document.querySelector('img.time')
+const iconSrc = document.querySelector('.icon img')
+
 
 const updateUI = (data) => {
   console.log(data)
@@ -9,14 +12,39 @@ const updateUI = (data) => {
 
   // update details
   details.innerHTML = `
-    <h5 class="text-xl font-semibold">${cityDetails.name}</h5>
+    <h5 class="text-xl font-semibold bg-white p-2 inline-block mt-4 rounded-lg">${cityDetails.name}</h5>
     <div class="capitalize text-lg">${weather.weather[0].description}</div>
-    <div class="pb-4 text-xl">
+    <div class="pb-4 text-3xl">
       <span>${(weather.main.temp - 273.15).toFixed(2)}</span>
       <span>&deg; C</span>
     </div>
   `
+  // update the night/day
+  const icon = weather.weather[0].icon //get icon d/n(day/night)
+  let timeSrc = null
+  const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+  if (icon[2] === 'd') {
+    timeSrc = './img/day.svg'
+  } else {
+    timeSrc = './img/night.svg'
+  }
+
+  time.setAttribute('src', timeSrc)
+  iconSrc.setAttribute('src', iconURL)
+
 }
+
+const cityUpdate = async (city) => {
+  const cityDetails = await getCity(city)
+  const weather = await getWeather(cityDetails.lat, cityDetails.lon)
+
+  return {
+    cityDetails: cityDetails,
+    weather: weather
+  }
+}
+
 // submit event
 cityForm.addEventListener('submit', e => {
   e.preventDefault()
